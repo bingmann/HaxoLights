9e9e4ca415d812400e2d82b9e159b1b552a3f504
Waweic <waweic@protonmail.com>
2018-07-26 20:47:23 +0200

    removed duplication

--- src/DMX_Send.ino
+++ src/DMX_Send.ino
@@ -71,21 +71,6 @@ public:
     }
 
     // RGBA colors
-    void set(size_t r, size_t g, size_t b, size_t a) {
-        for (size_t i = 0; i < 4; ++i) {
-            dmx.write(addr_ + 3 + 4 * i + 0, r);
-            dmx.write(addr_ + 3 + 4 * i + 1, g);
-            dmx.write(addr_ + 3 + 4 * i + 2, b);
-            dmx.write(addr_ + 3 + 4 * i + 3, a);
-        }
-    }
-
-    // RGBA colors
-    void set(const Color& c) {
-        set(c.r, c.g, c.b, c.w);
-    }
-
-    // RGBA colors
     void set_part(size_t p, size_t r, size_t g, size_t b, size_t a) {
         dmx.write(addr_ + 3 + 4 * p + 0, r);
         dmx.write(addr_ + 3 + 4 * p + 1, g);
@@ -97,6 +82,20 @@ public:
     void set_part(size_t p, const Color& c) {
         set_part(p, c.r, c.g, c.b, c.w);
     }
+
+    // RGBA colors
+    void set(size_t r, size_t g, size_t b, size_t a) {
+        for (size_t i = 0; i < 4; ++i) {
+            set_part(i, r, g, b, a);
+        }
+    }
+
+    // RGBA colors
+    void set(const Color& c) {
+        set(c.r, c.g, c.b, c.w);
+    }
+
+    
 
 private:
     size_t addr_;
